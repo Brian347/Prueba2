bb69316f012f1d68cb73ca487bb031282393d862
BRIAN <BRIAN@DESKTOP-FKUAPU1>
2019-09-05 18:50:14 -0500

    Prueba send1

---
+++ Ejercicios/Ejercicios_2019/src/ejercicios_2019/Ejercicio_1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios_2019;
+
+/**
+ *
+ * @author BRIAN
+ */
+public class Ejercicio_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola esto es una prueba desde comandos");
+        // TODO code application logic here
+    }
+    
+}
